84a08fe2e870f184ac3e79fc7584ce0173ca6c88
mgranella <94334357+mgranella@users.noreply.github.com>
2022-01-07 14:00:52 -0300

    Update book.component.ts

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -14,21 +14,12 @@ export class BookComponent implements OnInit {
   constructor(private apiService: ApiService, private router: Router) { }
 
   async ngOnInit() {
-  //ngOnInit(): void {
-    //this.books = await this.apiService.get(); --> THIS WORKS
-    //this.apiService.get().then<Book[]>(data => { --> <Book[]> NOT needed
     this.apiService.getAll().then(data => {
-      //console.log(`Async Inside prromise-1:: ${JSON.stringify(data)}`);  
-      //this.books = data.results;
       return this.books = data;
-      ////console.log(`this.book:: ${JSON.stringify(this.books)}`);  
     });
-    //console.log(`Async/Await:: Outside promise-1:: ${JSON.stringify(temp)}`);
-    console.log(`Outside promise-2`);
   }
 
   async showDetail(bookId:number){
-    console.log(`hey::${bookId}`);
     this.router.navigate(['detail',bookId]);
   }
 
